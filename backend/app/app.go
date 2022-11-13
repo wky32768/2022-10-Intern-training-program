8dc13727b50b6da357b92535b8883b78df938015
Keyi Wang <527192083@qq.com>
2022-11-13 20:56:02 +0800

    UPD_fix
    now we added database modifying APIs to our server.

--- backend/app/app.go
+++ backend/app/app.go
@@ -4,14 +4,16 @@ import (
 	"backend/app/controller/pong"
 	"backend/app/controller/print/query"
 	"backend/app/middleware"
+	"backend/model"
 )
 
 func AddRoutes() {
 	//api := e.Group("api")
-	//api.Use(middleware.Auth)
-	//api.GET("/ping", pong.Ping)
 
 	e.Use(middleware.Auth)
 	e.GET("/ping", pong.Ping)
 	e.POST("/print/query", query.Query)
+	e.POST("/db/create", model.Add)
+	e.POST("/db/search", model.Get)
+	e.POST("/db/delete", model.Delete)
 }
--- backend/model/init.go
+++ backend/model/init.go
@@ -1,14 +1,74 @@
 package model
 
 import (
+	"backend/app/response"
 	"fmt"
+	"github.com/labstack/echo/v4"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"net/http"
+	"strconv"
 )
 
 var DB *gorm.DB
+
+// Add 添加新的todo
+func Add(c echo.Context) error {
+	id, error1 := strconv.Atoi(c.QueryParam("Id"))
+	uid := uint(id)
+	if error1 != nil {
+		return response.SendResponse(c, http.StatusOK, "create failed", "error")
+	}
+	userid, error2 := strconv.Atoi(c.QueryParam("UserId"))
+	uintUserid := uint(userid)
+	if error2 != nil {
+		return response.SendResponse(c, http.StatusOK, "create failed", "error")
+	}
+	content := c.QueryParam("Content")
+	title := c.QueryParam("Title")
+	var newTodo = Todos{Id: uid, UserId: uintUserid, Content: content, Title: title}
+	err := DB.Create(&newTodo).Error
+	if err != nil {
+		return response.SendResponse(c, http.StatusOK, "create failed", "error")
+	} else {
+		return response.SendResponse(c, http.StatusOK, "create succeeded", "newTodo.id")
+	}
+}
+
+// Get 查询todo
+func Get(c echo.Context) error {
+	id, error1 := strconv.Atoi(c.QueryParam("Id"))
+	uid := uint(id)
+	if error1 != nil {
+		return response.SendResponse(c, http.StatusOK, "get failed", "error")
+	}
+	var result Todos
+	err := DB.Where("user_id =?", uid).Find(&result).Error
+	if err != nil {
+		return response.SendResponse(c, http.StatusOK, "get failed", "error")
+	} else {
+		return response.SendResponse(c, http.StatusOK, "Todos", "get succeeded")
+	}
+}
+
+// Delete 删除todo
+func Delete(c echo.Context) error {
+	id, error1 := strconv.Atoi(c.QueryParam("Id"))
+	uid := uint(id)
+	if error1 != nil {
+		return response.SendResponse(c, http.StatusOK, "delete failed", "error")
+	}
+	var result Todos
+	err := DB.Where("user_id =?", uid).Find(&result).Error
+	if err != nil {
+		return response.SendResponse(c, http.StatusOK, "delete failed", "error")
+	} else {
+		DB.Where("user_id =?", uid).Delete(result)
+		return response.SendResponse(c, http.StatusOK, "result", "delete succeeded")
+	}
+}
 
 // Init 连接数据库中把Users与Todos放入数据表
 func Init() {
